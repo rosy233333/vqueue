5646008d4023a3176ddc8b5c6684fcf8821fc6d5
rosy <lc_rosy@qq.com>
2025-11-28 16:18:26 +0800

    使用mut_cfg!宏定义可被环境变量指定的常量

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    vdso_helper::mut_cfg! {
+        const QUEUE_LEN: usize = 1024;
+        const ARRAY_LEN: usize = 64;
+        // const BOOL_TEST: bool = true;
+        // const EXPR_TEST: usize = QUEUE_LEN / 2;
+        // const FLOAT_TEST: f64 = 3.14159;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,23 @@
 
 mod deque;
 mod slot_array;
+
+vdso_helper::use_mut_cfg! {}
+
+#[cfg(test)]
+mod test_mut_cfg {
+    extern crate std;
+
+    use super::*;
+    use std::println;
+
+    // run with `cargo test test_constants -- --nocapture`
+    #[test]
+    fn test_constants() {
+        println!("QUEUE_LEN: {}", QUEUE_LEN);
+        println!("ARRAY_LEN: {}", ARRAY_LEN);
+        // println!("BOOL_TEST: {}", BOOL_TEST);
+        // println!("EXPR_TEST: {}", EXPR_TEST);
+        // println!("FLOAT_TEST: {}", FLOAT_TEST);
+    }
+}
