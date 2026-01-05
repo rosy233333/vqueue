119d90769e4b02603eb59317a594e2d9486a329d
rosy <lc_rosy@qq.com>
2026-01-05 14:57:05 +0800

    更新IPCItem结构体，添加rep_type字段以支持消息派发到协程。

--- src/ipc_item.rs
+++ src/ipc_item.rs
@@ -1,7 +1,16 @@
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
 pub struct IPCItem {
+    /// 发送者的entity id，标识进程
     pub sender: u64,
+    /// 消息类型，用于选择接收者处理消息的协程
     pub msg_type: u64,
+    /// 若该消息需要回复，则回复消息发送给sender，且msg_type设为此消息的rep_type。
+    ///
+    /// 一般设为等待消息回复的协程id。
+    ///
+    /// 不需回复的消息可忽略此字段。
+    pub rep_type: u64,
+    /// 消息数据
     pub data: [u64; 8],
 }
