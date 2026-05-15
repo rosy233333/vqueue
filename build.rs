0442789dddcb7ec7006f990cd9cd97b32e483538
rosy <lc_rosy@qq.com>
2026-05-15 21:44:12 +0800

    为mut_cfg添加注释；
    更新vdso_crate_template

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 fn main() {
     vdso_helper::mut_cfg! {
+        /// 单条队列长度
         const QUEUE_LEN: usize = 4096;
+        /// 数组长度，决定同时可用的队列数量
         const ARRAY_LEN: usize = 64;
-        // const BOOL_TEST: bool = true;
-        // const EXPR_TEST: usize = QUEUE_LEN / 2;
-        // const FLOAT_TEST: f64 = 3.14159;
     }
 }
